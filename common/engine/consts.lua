7ea42c5aa277f0e1b2830f2518f5b7c26ef2ca90
Endaris <Endaris@users.noreply.github.com>
2025-02-21 21:57:08 +0100

    bump engine version to 049

--- common/engine/consts.lua
+++ common/engine/consts.lua
@@ -30,8 +30,9 @@ consts.ENGINE_VERSIONS.PRE_TELEGRAPH = "045"
 consts.ENGINE_VERSIONS.TELEGRAPH_COMPATIBLE = "046"
 consts.ENGINE_VERSIONS.TOUCH_COMPATIBLE = "047"
 consts.ENGINE_VERSIONS.LEVELDATA = "048"
+consts.ENGINE_VERSIONS.WIGGLE_PUNISH = "049"
 
-consts.ENGINE_VERSION = consts.ENGINE_VERSIONS.LEVELDATA -- The current engine version
+consts.ENGINE_VERSION = consts.ENGINE_VERSIONS.WIGGLE_PUNISH -- The current engine version
 consts.VERSION_MIN_VIEW = consts.ENGINE_VERSIONS.LEVELDATA -- The lowest version number that can be watched
 
 consts.COUNTDOWN_CURSOR_SPEED = 4 --one move every this many frames
